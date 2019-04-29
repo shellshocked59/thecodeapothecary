36b029a100e02ee5b489912b53b367bcb09df15e
Eric Washburn <ewashburn88@gmail.com>
2019-04-29 11:57:30 -0600

    wip

--- node/config/development.js
+++ node/config/development.js
@@ -2,7 +2,7 @@ exports.get_settings = function(){
 	var settings = {};
 	settings.PORT = '3000';
 	settings.ENDPOINT = 'http://www.thecodeapothecary.com:8080/';
-	settings.ENDPOINT_PORT = '80';
+	settings.ENDPOINT_PORT = '8080';
 	settings.SITE_KEY = 'tca';
 
 	return settings;
