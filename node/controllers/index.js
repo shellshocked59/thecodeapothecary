0b6b88c042a6a0999e5173477a72875308df78e4
Eric Washburn <ewashburn88@gmail.com>
2019-05-13 15:09:31 -0600

    adding metatags

--- node/controllers/index.js
+++ node/controllers/index.js
@@ -3,21 +3,27 @@ var redis_model = require('../models/redis');
 exports.home = function(req, res) {
 	let header_footer_data = this.get_header_footer_data();
 	let json_data = redis_model.get_json_node('/home');
+	let metadata = this.getMetadata(json_data, header_footer_data);
     res.render('index', { 
     	header_footer: header_footer_data, 
     	title: json_data['title'][0]['value'], 
-    	json_data: json_data 
+    	description: json_data['field_meta_description'][0]['value'],
+    	json_data: json_data,
+    	metadata: metadata
     });
 }
 
 exports.default_route = function(req, res) {
 	let header_footer_data = this.get_header_footer_data();
 	let json_data = redis_model.get_json_node(req.path);
+	let metadata = this.getMetadata(json_data, header_footer_data);
 	if(json_data != undefined){
 		res.render('index', { 
 			header_footer: header_footer_data, 
 			title: json_data['title'][0]['value'], 
-			json_data: json_data 
+			description: json_data['field_meta_description'][0]['value'],
+			json_data: json_data,
+			metadata: metadata
 		});
 	}else{
 		res.send(404);
@@ -94,5 +100,23 @@ exports.get_header_footer_data = function(req, res) {
 		'site_title': redis_model.get_json_key('title')[0]['value'],
 		'site_subtitle':redis_model.get_json_key('field_tagline')[0]['value'],
 		'site_copyright':redis_model.get_json_key('field_copyright')[0]['value'],
+		'site_domain': redis_model.get_json_key('field_primary_domain')[0]['value'],
+	};
+}
+
+exports.getMetadata = function(json_data, header_footer_data) {
+	let site_domain = 'http://' + header_footer_data.site_domain;
+	let path = json_data.path[0]['alias'];
+	if(path == 'home'){
+		path = '';
+	}
+	return {
+		'robots': 'follow, index',
+		'url': site_domain + path, 
+		'title': json_data.title[0]['value'],
+		'description': json_data.field_meta_description[0]['value'],
+		'site_title': header_footer_data.site_title,
+		'site_domain': site_domain,
+		'site_copyright': header_footer_data.site_copyright,
 	};
 }
